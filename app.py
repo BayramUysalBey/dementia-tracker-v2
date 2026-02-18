fcfc49c38a4f2ffd9acb0d8ef91972c0f8184b3e
BayramUysalBey <bayramuysal@windowslive.com>
2026-02-18 16:26:28 +0300

    The ability to serve static files has been added for the 'static' directory. A templates folder has been created (for placeholder purposes).

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 class HealthStatus(BaseModel):
 	status: str
